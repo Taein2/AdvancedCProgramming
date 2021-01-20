c54d085c7739c9aa5834d904ae9f9e672604dd30
TaeinKim <66376774+Taein2@users.noreply.github.com>
2021-01-21 05:17:17 +0900

    구구단 출력하기

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	for (int i = 2; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
+			printf("%d * %d = %-2d  ", i, j, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
